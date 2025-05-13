38fa0db2ccb044e224ed08bac410f1d8fca731c2
DouglasReis98 <y*********@gmail.com>
2025-05-12 22:33:17 -0300

    inserir_dados_localStorage

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,28 +1,96 @@
-import React from 'react'
-import style from './Form.module.css'
+import React, { useState } from "react";
+import style from "./Form.module.css";
 const Form = () => {
-  return (
-    <form>
-        <div>
-            <label for="item">Item:</label>
-            <input type="text" id={style.item}/>
-        </div>
+  const [item, setItem] = useState("");
+  const [qtde, setQtde] = useState("");
+  const [preco, setPreco] = useState("");
 
-        <div>
-            <label for="qtde">Qtde:</label>
-            <input type="number" min="0" id={style.qtde}/>
-        </div>
+  let arrItens = [];
+  let editIndex = null;
 
-        <div>
-            <label for="preco">Preço (R$):</label>
-            <input type="number" step="0.010" id={style.preco}/>
-        </div>
-        <div id={style.botoes}>
-            <input type="submit" id={style.addItem} value="Adicionar"/>
-            <input type="reset" id={style.resetItem} value="Limpar"/>
-        </div>
-    </form>
-  )
+if (localStorage.getItem("itensOrcamento")) {
+  arrItens = JSON.parse(localStorage.getItem("itensOrcamento"));
 }
 
-export default Form
+  const cadastrarItem = (e) => {
+    e.preventDefault();
+
+    if (item !== "" && qtde !== isNaN && preco !== isNaN) {
+    if (editIndex === null) {
+      const objItem = {
+        Item: item,
+        Quantidade: qtde,
+        Preco: (preco * qtde).toFixed(2),
+      };
+
+      arrItens.push(objItem);
+
+      localStorage.setItem("itensOrcamento", JSON.stringify(arrItens));
+
+    } else {
+      arrItens[editIndex] = {
+        Item: item,
+        Quantidade: qtde,
+        Preco: (preco * qtde).toFixed(2),
+      };
+
+      arrItens.splice(editIndex, 1, arrItens[editIndex]);
+      localStorage.setItem("itensOrcamento", JSON.stringify(arrItens));
+      editIndex = null;
+      //form.removeChild(form.children[0])
+      //addItem.value = "Adicionar";
+
+    }
+    
+    setItem("");
+    setQtde("");
+    setPreco("");
+
+    //exibirOrcamento();
+  } else {
+    alert("Os dados precisam ser inseridos!");
+  }
+
+    console.log(item + " " + qtde + " " + preco + " ");
+
+  };
+
+  return (
+    <form onSubmit={cadastrarItem}>
+      <div>
+        <label htmlFor="item">Item:</label>
+        <input
+          type="text"
+          id={style.item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="qtde">Qtde:</label>
+        <input
+          type="number"
+          min="0"
+          id={style.qtde}
+          onChange={(e) => setQtde(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="preco">Preço (R$):</label>
+        <input
+          type="number"
+          step="0.010"
+          id={style.preco}
+          onChange={(e) => setPreco(e.target.value)}
+        />
+      </div>
+      <div id={style.botoes}>
+        <input type="submit" id={style.addItem} value="Adicionar" />
+        <input type="reset" id={style.resetItem} value="Limpar" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
--- src/components/Tabela.js
+++ src/components/Tabela.js
@@ -1,40 +1,38 @@
-import React from 'react'
-import style from './Tabela.module.css'
+import React from "react";
+import style from "./Tabela.module.css";
 const Tabela = () => {
   return (
     <>
-    <table id={style.tbOrcamento} border="2" align="center">
-<thead>
-            <tr>
-                <th colspan="5">Itens Adicionados</th>
-            </tr>
-            <tr>
-
-                <th>Item</th>
-                <th>Quantidade</th>
-                <th>Preço</th>
-                <th colspan="2">Ação</th>
-            </tr>
+      <table id={style.tbOrcamento} border="2" align="center">
+        <thead>
+          <tr>
+            <th colSpan="5">Itens Adicionados</th>
+          </tr>
+          <tr>
+            <th>Item</th>
+            <th>Quantidade</th>
+            <th>Preço</th>
+            <th colSpan="2">Ação</th>
+          </tr>
         </thead>
         <tbody></tbody>
-<tfoot>
-            <tr>
-                <td colspan="5">
-                    <p>Total: R$ <span id={style.total}>0,00</span></p>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="5">
-                    <button id={style.baixar}>Baixar Arquivo</button>
-                </td>
-            </tr>
-
-
+        <tfoot>
+          <tr>
+            <td colSpan="5">
+              <p>
+                Total: R$ <span id={style.total}>0,00</span>
+              </p>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan="5">
+              <button id={style.baixar}>Baixar Arquivo</button>
+            </td>
+          </tr>
         </tfoot>
-
-    </table>
+      </table>
     </>
-  )
-}
+  );
+};
 
-export default Tabela
+export default Tabela;
